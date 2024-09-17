edd82d918f73fe3301f5021750e35f2162a278ca
Gowthami Allu <gowthami.allu@Gowthami-Allu-C02D5HZSMD6M.local>
2024-09-17 16:05:52 +0530

    Added two more device

--- movingTech.NY/User/Android/DevicePermission.java
+++ movingTech.NY/User/Android/DevicePermission.java
@@ -9,9 +9,10 @@ import io.appium.java_client.AppiumBy;
 
 public class DevicePermission extends BaseClass {
 
+	//	 String deviceUdid = actualUdid;
+
 	public static void autoSuggestionDecline() {
-		System.out.println(udids.get(2));
-		if (Arrays.asList("15913008960024W", "RFCT42T5N9D", "SONJFMDUKV65OJV4", "28301JEGR03129", "R9ZTB0KD0FA", "1371785921000CT", "RZ8N91FWYXT").contains(udids.get(2))) {
+		if (Arrays.asList("15913008960024W", "RFCT42T5N9D", "SONJFMDUKV65OJV4", "28301JEGR03129", "1371785921000CT", "RZ8N91FWYXT", "RZ8R72RE6AB", "9618349153000CS(").contains(udids.get(2))) {
 
 			driver1.findElement(AppiumBy.xpath("//android.widget.Button[@text='NONE OF THE ABOVE']")).click();
 
@@ -27,12 +28,12 @@ public class DevicePermission extends BaseClass {
 
 	public static void locationPermissionsConfirmation() {
 
-		if(Arrays.asList("15913008960024W", "1377682723004YR", "1371785921000CT").contains(udids.get(2)))
+		if(Arrays.asList("15913008960024W", "1377682723004YR", "1371785921000CT", "9618349153000CS(").contains(udids.get(2)))
 		{
 			driver1.findElement(AppiumBy.xpath("//android.widget.Button[@text='WHILE USING THE APP']")).click();
 
 		}
-		else if (Arrays.asList("8cb2f113", "RFCT42T5N9D", "SONJFMDUKV65OJV4", "28301JEGR03129", "R9ZTB0KD0FA", "RZ8N91FWYXT").contains(udids.get(2))) {
+		else if (Arrays.asList("8cb2f113", "RFCT42T5N9D", "SONJFMDUKV65OJV4", "28301JEGR03129", "R9ZTB0KD0FA", "RZ8N91FWYXT", "RZ8R72RE6AB").contains(udids.get(2))) {
 
 			driver1.findElement(AppiumBy.xpath("//android.widget.Button[@text='While using the app']")).click();
 
@@ -44,13 +45,13 @@ public class DevicePermission extends BaseClass {
 	}
 
 	public static void notification() {
-		if(Arrays.asList("1371785921000CT", "15913008960024W").contains(udids.get(2)))
+		if(Arrays.asList("1371785921000CT", "15913008960024W", "9618349153000CS(").contains(udids.get(2)))
 		{
 
 			driver1.findElement(AppiumBy.xpath("//android.widget.Button[@text='ALLOW']")).click();
 
 		}
-		else if (Arrays.asList("RZ8N91FWYXT").contains(udids.get(2)))
+		else if (Arrays.asList("RZ8N91FWYXT", "R9ZTB0KD0FA").contains(udids.get(2)))
 		{
 			driver1.findElement(AppiumBy.xpath("//android.widget.Button[@text='Allow']")).click();
 
