3449505ce9fb15e4b345be0df92f2bffe09eb751
ajay <aj05kumar1994@gmail.com>
2024-09-23 16:31:52 +0530

    adding chnages for estimate screen and other screns

--- movingTech.NY/User/Android/RideSearch.java
+++ movingTech.NY/User/Android/RideSearch.java
@@ -4,19 +4,29 @@ import base.BaseClass;
 import io.appium.java_client.AppiumBy;
 
 public class RideSearch extends BaseClass{
-
+	
 	public void SearchForRideFromSuggestion() throws InterruptedException {
-		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Where are you going?']")).click();//new UI
-//		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Where to?']")).click();
+		System.out.println("Ride Search about to start");
+		Thread.sleep(3000);
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Where to?']")).click();
 		Thread.sleep(2000);
-		driver1.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Where to?']")).sendKeys("Keng");
+		driver1.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Where to?']")).sendKeys("Koram");
 		Thread.sleep(2000);
-		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Kengeri']")).click();
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Koramangala']")).click();
 		Thread.sleep(2000);
 		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Confirm Location']")).click();
+		Thread.sleep(4000);
 
+		
 	}
-	
 
-
+	public void SearchForRideForIntercity() throws InterruptedException {
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Where to?']")).click();
+		Thread.sleep(2000);
+		driver1.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Where to?']")).sendKeys("Tumu");
+		Thread.sleep(2000);
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Tumukuru']")).click();
+		Thread.sleep(2000);
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Confirm Location']")).click();
+	}
 }
